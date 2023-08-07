f7c4ea602d5e158969d86fe45681e94cbe354e62
KS Kim <65766852+simeddk@users.noreply.github.com>
2023-08-07 19:54:24 +0900

    0807_FPS

---
+++ Source/U05_Session/FP_FirstPerson/FP_FirstPersonCharacter.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "FP_FirstPersonCharacter.generated.h"
+
+class UInputComponent;
+class UCameraComponent;
+class USkeletalMeshComponent;
+class USoundBase;
+class UAnimMontage;
+
+UCLASS(config=Game)
+class AFP_FirstPersonCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+//Components
+	UPROPERTY(VisibleDefaultsOnly, Category=Mesh)
+		USkeletalMeshComponent* FP_Mesh;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+		USkeletalMeshComponent* FP_Gun;
+
+	UPROPERTY(VisibleDefaultsOnly)
+		class USkeletalMeshComponent* TP_Gun;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		UCameraComponent* Camera;
+
+public:
+	AFP_FirstPersonCharacter();
+
+//Properties
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
+		float BaseTurnRate;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
+		float BaseLookUpRate;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Gameplay)
+		USoundBase* FireSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+		UAnimMontage* FireAnimation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+		UAnimMontage* TP_FireAnimation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+		float WeaponRange;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+		float WeaponDamage;
+
+protected:
+	void OnFire();
+
+	void MoveForward(float Val);
+	void MoveRight(float Val);
+
+	void TurnAtRate(float Rate);
+	void LookUpAtRate(float Rate);
+
+	FHitResult WeaponTrace(const FVector& StartTrace, const FVector& EndTrace) const;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+
+
+public:
+	FORCEINLINE class USkeletalMeshComponent* GetFP_Mesh() const { return FP_Mesh; }
+	FORCEINLINE class UCameraComponent* GetFirstPersonCameraComponent() const { return Camera; }
+
+};
+
